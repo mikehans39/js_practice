e0f5eb9ca6b834772f15aaedae2f01116ff51777
Michael Hansen 02 <mikehans39@gmail.com>
2020-05-24 21:56:35 -0500

    additional code added to practice_3.js

--- practice_3.js
+++ practice_3.js
@@ -39,3 +39,105 @@ const greetAliens = string => {
 const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
 
 greetAliens(aliens);
+
+
+
+const convertToBaby = line => {
+  let newArray = [];
+  for (let i = 0; i < line.length; i++) {
+    let entry = 'baby ' + line[i];
+    newArray.push(entry); 
+  }
+  return newArray;
+}
+
+
+// When you're ready to test your code, uncomment the below and run:
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals)) 
+
+
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = (veg) => {
+      console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+}
+
+// Write your code here:
+
+const declineEverything = string => {
+  string.forEach(politelyDecline);
+}
+
+const acceptEverything = veg => {
+  veg.forEach(items => console.log(`Ok, I guess I will eat some ${items}.`));
+}
+
+declineEverything(veggies);
+acceptEverything(veggies);
+
+
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+
+const squareNums = numbers => {
+  return numbers.map(x => x * x);
+}
+
+console.log(squareNums(numbers));
+
+
+
+
+// Write your code here:
+
+const shoutGreetings = string => {
+  let newArray = [];
+  let newList = string.map(x => x + '!');
+  for (let i = 0; i < newList.length; i++) {
+    let newItem = newList[i];
+    newArray.push(newItem.toUpperCase());
+  } 
+  return newArray;
+}
+
+
+
+// Feel free to uncomment out the code below to test your function!
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings));
+// Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
+
+
+
+
+
+
+// Write your code here:
+
+const sortYears = list => {
+  return list.sort().reverse();
+}
+
+// Feel free to uncomment the below code to test your function:
+
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+
+console.log(sortYears(years))
+// Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
+
+
+
+
+
+
+
+
+
